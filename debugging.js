9363a6863aac358a853ae016c4e3a9024d5bd145
Bob Remeika <bob.remeika@gmail.com>
2017-12-14 15:30:44 -0800

    Added comments

--- debugging.js
+++ debugging.js
@@ -1,15 +1,27 @@
 
+// keys are pair names and the arrays represent the value of a pair at a point in time
 const pairs = {
   'eth_usd': [112, 120, 9000],
   'btc_usd': [1120, 1200, 7000],
   'vti_usd': [{ price: 120, div: 20 }, { price: 200, div: 40 }, { price: 750, div: 75 }]
 }
 
+/***
+ * Finds the best performing pair for a given time period
+ *
+ * @pairs The pairs datastructure
+ * @start Starting index
+ * @end   End index
+ *
+ * @returns An object with information about the best performing pair
+ */
 const findBestPair = (pairs, [start, end]) => {
+  const maxSlopeReducer = (acc, cmp) => acc = (cmp.slope > acc.slope) ? cmp : acc
+
   return Object.entries(pairs).map(([pair, data]) => {
     const slope = (data[end] - data[start]) / (end - start);
     return {pair, slope};
-  }).reduce((acc, cmp) => acc = (cmp.slope > acc.slope) ? cmp : acc, {pair: '', slope: 0});
+  }).reduce((acc, cmp) => (cmp.slope > acc.slope) ? cmp : acc, {pair: '', slope: 0});
 }
 
 const bestPair = findBestPair(pairs, [0, 2]);
